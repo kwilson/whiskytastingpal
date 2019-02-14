718d80df159f6ebff7800bfb5c7071511f1ef106
Kevin Wilson <kevin@partcoffee.com>
2019-02-14 14:15:58 +0000

    Extract side-effects from app

--- web/src/components/app/app.tsx
+++ web/src/components/app/app.tsx
@@ -1,37 +1,25 @@
 import * as React from 'react'
 import { useState, useEffect } from 'react';
-import { Subject, Observable } from 'rxjs';
-import { scan, startWith, map, switchMap, catchError, takeUntil, filter } from 'rxjs/operators';
-import { ajax, AjaxError } from 'rxjs/ajax';
+import { Subject } from 'rxjs';
+import { scan, startWith } from 'rxjs/operators';
 
 import { reducer, INITIAL_STATE } from '../../reducers';
 import { SearchForm } from '../search-form';
-import { combineEffects, ofType } from '../../effects/utils';
+import { combineEffects } from '../../effects/utils';
 import { SearchResults } from '../search-results';
 import { ISearchResult } from '../../types/search-result';
 import { Loader } from '../loader';
 import { Details } from '../details';
 import {
-    loadSearchResults,
-    loadSearchResultsFulfilled,
-    loadSearchResultsRejected,
-    loadDetails,
-    loadDetailsFulfilled,
-    loadDetailsRejected,
     changeSearchQuery,
     hideResult,
     showResult,
     clear,
-    ActionType,
-    IChangeSearchQueryAction,
-    ILoadSearchResultsAction,
-    IShowResultAction,
-    ILoadDetailsAction,
     Actions
 } from '../../actions';
-import { IDetails } from '../../types/details';
 
 import './app.scss';
+import { queryChangeEffect$, loadSearchResultsEffect$, selectResultEffect$, loadDetailsEfect$ } from '../../effects';
 
 const action$ = new Subject<Actions>();
 
@@ -54,57 +42,11 @@ export const App: React.StatelessComponent<{}> = () => {
     }, []);
 
     useEffect(() => {
-        const queryChangeEffect$: Observable<Actions> = action$.pipe(
-            ofType<IChangeSearchQueryAction>(ActionType.CHANGE_SEARCH_QUERY),
-            filter(({ payload }) => Boolean(payload.searchQuery)),
-            map<IChangeSearchQueryAction, ILoadSearchResultsAction>(({ payload }) => loadSearchResults(payload.searchQuery))
-        );
-
-        const loadSearchResultsEffect$: Observable<Actions> = action$.pipe(
-            ofType<ILoadSearchResultsAction>(ActionType.LOAD_SEARCH_RESULTS),
-            map((action) => action.payload.searchQuery),
-            switchMap(
-                (searchQuery: string) => ajax.getJSON<ISearchResult[]>(`/api/search?terms=${searchQuery}`).pipe(
-                    map((results) => loadSearchResultsFulfilled(results)),
-                    takeUntil(action$.pipe(
-                        ofType(
-                            ActionType.LOAD_SEARCH_RESULTS,
-                            ActionType.CLEAR
-                        )
-                    )),
-                    catchError((_: AjaxError) => loadSearchResultsRejected)
-                )
-            )
-        );
-
-        const selectResultEffect$: Observable<Actions> = action$.pipe(
-            ofType<IShowResultAction>(ActionType.SHOW_RESULT),
-            map(({ payload }) => loadDetails(payload.selectedResult))
-        );
-
-        const loadDetailsEfect$: Observable<Actions> = action$.pipe(
-            ofType<ILoadDetailsAction>(ActionType.LOAD_DETAILS),
-            map((action) => action.payload.selectedResult.url),
-            switchMap(
-                (url: string) => ajax.getJSON<IDetails>(`/api/${url}`).pipe(
-                    map((details) => loadDetailsFulfilled(details)),
-                    takeUntil(action$.pipe(
-                        ofType(
-                            ActionType.LOAD_DETAILS,
-                            ActionType.CHANGE_SEARCH_QUERY,
-                            ActionType.CLEAR
-                        )
-                    )),
-                    catchError((_: AjaxError) => loadDetailsRejected)
-                )
-            )
-        );
-
         const effects$ = combineEffects(
-            queryChangeEffect$,
-            loadSearchResultsEffect$,
-            selectResultEffect$,
-            loadDetailsEfect$
+            queryChangeEffect$(action$),
+            loadSearchResultsEffect$(action$),
+            selectResultEffect$(action$),
+            loadDetailsEfect$(action$)
         );
 
         const subscription = effects$.subscribe((x) => action$.next(x));
---
+++ web/src/effects/index.ts
@@ -0,0 +1,53 @@
+import { Observable } from 'rxjs';
+import { filter, map, switchMap, takeUntil, catchError } from 'rxjs/operators';
+import { ofType } from './utils';
+import { Actions, IChangeSearchQueryAction, ActionType, ILoadSearchResultsAction, loadSearchResults, loadSearchResultsFulfilled, loadSearchResultsRejected, IShowResultAction, loadDetails, ILoadDetailsAction, loadDetailsFulfilled, loadDetailsRejected } from '../actions';
+import { ajax, AjaxError } from 'rxjs/ajax';
+import { ISearchResult } from '../types/search-result';
+import { IDetails } from '../types/details';
+
+export const queryChangeEffect$ = (action$: Observable<Actions>): Observable<Actions> => action$.pipe(
+    ofType<IChangeSearchQueryAction>(ActionType.CHANGE_SEARCH_QUERY),
+    filter(({ payload }) => Boolean(payload.searchQuery)),
+    map<IChangeSearchQueryAction, ILoadSearchResultsAction>(({ payload }) => loadSearchResults(payload.searchQuery))
+);
+
+export const loadSearchResultsEffect$ = (action$: Observable<Actions>): Observable<Actions> => action$.pipe(
+    ofType<ILoadSearchResultsAction>(ActionType.LOAD_SEARCH_RESULTS),
+    map((action) => action.payload.searchQuery),
+    switchMap(
+        (searchQuery: string) => ajax.getJSON<ISearchResult[]>(`/api/search?terms=${searchQuery}`).pipe(
+            map((results) => loadSearchResultsFulfilled(results)),
+            takeUntil(action$.pipe(
+                ofType(
+                    ActionType.LOAD_SEARCH_RESULTS,
+                    ActionType.CLEAR
+                )
+            )),
+            catchError((_: AjaxError) => loadSearchResultsRejected)
+        )
+    )
+);
+
+export const selectResultEffect$ = (action$: Observable<Actions>): Observable<Actions> => action$.pipe(
+    ofType<IShowResultAction>(ActionType.SHOW_RESULT),
+    map(({ payload }) => loadDetails(payload.selectedResult))
+);
+
+export const loadDetailsEfect$ = (action$: Observable<Actions>): Observable<Actions> => action$.pipe(
+    ofType<ILoadDetailsAction>(ActionType.LOAD_DETAILS),
+    map((action) => action.payload.selectedResult.url),
+    switchMap(
+        (url: string) => ajax.getJSON<IDetails>(`/api/${url}`).pipe(
+            map((details) => loadDetailsFulfilled(details)),
+            takeUntil(action$.pipe(
+                ofType(
+                    ActionType.LOAD_DETAILS,
+                    ActionType.CHANGE_SEARCH_QUERY,
+                    ActionType.CLEAR
+                )
+            )),
+            catchError((_: AjaxError) => loadDetailsRejected)
+        )
+    )
+);
