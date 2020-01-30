2ac7fa6503f4e02a1383075358dea6d4f171e8fd
Kevin Wilson <kevin@partcoffee.com>
2020-01-30 21:53:48 +0000

    Tweak build config

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve('./dist'),
         publicPath: '/'
     },
     module: {
