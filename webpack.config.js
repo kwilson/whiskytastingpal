5acc2bf73c53cfe84d7c0f656dd1dff391c8b181
Kevin Wilson <kevin@partcoffee.com>
2020-01-30 21:20:40 +0000

    Start port to Netlify

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
+const themeColor = '#505465';
+const siteTitle = 'Whisky Tasting Pal';
+const siteDescription = 'Finding tasting notes for your favourite drams.';
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "babel-loader" }
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: siteTitle,
+            description: siteDescription,
+            themeColor,
+            template: './src/index.html',
+        }),
+        new CopyPlugin([
+            { from: './static', to: './' },
+        ]),
+        new CleanWebpackPlugin(),
+    ]
+};
