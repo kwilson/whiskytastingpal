0d0c0b53c881229fa9f0581e339063f1b3ec502e
Kevin Wilson <kevin@partcoffee.com>
2020-01-31 19:23:16 +0000

    Add POC call to function

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,30 @@
 import * as React from 'react';
+import { get } from 'axios';
 
-export const App = () => <p>Hello World</p>;
+export const App = () => {
+    const [terms, updateTerms] = React.useState('');
+    const [result, updateResults] = React.useState({});
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        get(`/.netlify/functions/search?terms=${terms}`)
+        .then(results => {
+            updateResults(results.data);
+        });
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSearch}>
+                <input value={terms} onChange={e => updateTerms(e.target.value)} />
+                <button type="submit">Search</button>
+            </form>
+
+            <hr />
+
+            <pre>
+                {JSON.stringify(result, null, 2)}
+            </pre>
+        </div>
+    )
+};
